c99206bbe09ed155ba03648b425e8b656e3d81de
akshayhegde <xan1790@gmail.com>
2012-10-29 15:51:15 -0700

    completing ex3

--- Chapter16/basename.m
+++ Chapter16/basename.m
@@ -6,12 +6,19 @@
 int main()
 {
     @autoreleasepool {
-        NSFileManager *fm = [NSFileManager defaultManager];
-        NSString *fileName;
+        NSString *fileName, *baseName;
         NSProcessInfo *proc = [NSProcessInfo processInfo];
         NSArray *args = [proc arguments];
 
+        if ([args count] != 2) {
+            NSLog(@"usage: basename path");
+            return 1;
+        }
+
         fileName = [args objectAtIndex:1];
+        baseName = [fileName lastPathComponent];
+
+        NSLog(@"%@", baseName);
     }
 
     return 0;
--- Chapter16/dirname.m
+++ Chapter16/dirname.m
@@ -6,12 +6,19 @@
 int main()
 {
     @autoreleasepool {
-        NSFileManager *fm = [NSFileManager defaultManager];
-        NSString *fileName;
+        NSString *fileName, *dirName;
         NSProcessInfo *proc = [NSProcessInfo processInfo];
         NSArray *args = [proc arguments];
 
+        if ([args count] != 2) {
+            NSLog(@"usage: dirname path");
+            return 1;
+        }
+
         fileName = [args objectAtIndex:1];
+        dirName = [fileName stringByDeletingLastPathComponent];
+
+        NSLog(@"%@", dirName);
     }
 
     return 0;
