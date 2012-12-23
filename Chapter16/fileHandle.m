cb684d1e8bf3e19da290c33e26a81aa9f05052c2
akshayhegde <xan1790@gmail.com>
2012-12-23 00:51:12 -0800

    completed exercise 6

--- Chapter16/fileHandle.m
+++ Chapter16/fileHandle.m
@@ -1,14 +1,19 @@
 // fileHandle.m
 // Modifies Program 16.7 so that the file is read and written kBufSize bytes
 // at a time. kBufSize is definied at the beginning of the program
+//
+// To test and compile the program, run the following in a term (without the $):
+// $ touch testfile
+// $ clang -fobjc-arc -framework Foundation fileHandle.m -o ./testFileHandle
+// $ ./testFileHandle
 #import <Foundation/Foundation.h>
 
 int main()
 {
     @autoreleasepool {
         NSFileHandle *inFile, *outFile;
-        NSData *buffer;
-        int kBufSize = 64;
+        unsigned long long kBufSize = 1000;
+        unsigned long long inputFileSize;
 
         // Open the file testfile for reading
         inFile = [NSFileHandle fileHandleForReadingAtPath: @"testfile"];
@@ -16,6 +21,10 @@ int main()
             NSLog(@"Open of testfile for reading failed.");
             return 1;
         }
+
+        // Get the file size and adjust the kBufSize
+        inputFileSize = [inFile seekToEndOfFile];
+
         // Create the output file if necessary
         [[NSFileManager defaultManager] createFileAtPath:@"testout"
                                                 contents:nil
@@ -28,19 +37,24 @@ int main()
             return 2;
         }
 
-        // truncase the output file since it may contain data
-        [outFile truncateFileAtOffset:0];
-
-        // Read the data from inFile and write it to outFile
-        buffer = [inFile readDataToEndOfFile];
-
-        [outFile writeData:buffer];
+        int iterations = (int) inputFileSize / kBufSize;
+        for (int i = 0; i <= iterations; i++) {
+            if (i == iterations) {
+                [inFile seekToFileOffset:kBufSize * i];
+                [outFile writeData:[inFile readDataToEndOfFile]];
+            }
+            else {
+                [inFile seekToFileOffset:kBufSize * i];
+                [outFile writeData:[inFile readDataOfLength:kBufSize]];
+            }
+        }
 
         // close the two files
         [inFile closeFile];
         [outFile closeFile];
 
         // Verify the file's contents
+        NSLog(@"Output file successfully created with testfile contents: ");
         NSLog(@"%@", [NSString stringWithContentsOfFile:@"testout"
                                                 encoding:NSUTF8StringEncoding
                                                     error:NULL]);
