74971da6b0ed6ee076f0d9a966dfdd5b6168f0e8
akshayhegde <xan1790@gmail.com>
2012-12-23 16:30:51 -0800

    completed ex2.m

--- Chapter19/ex1.m
+++ Chapter19/ex1.m
@@ -1,3 +1,5 @@
+// ex1.m -- Generates a table of prime numbers and stores it in an array
+// which is then stored inside a primes.pl XML(plist) file.
 #import <Foundation/Foundation.h>
 
 int main(int argc, char *argv[])
@@ -10,22 +12,17 @@ int main(int argc, char *argv[])
         for (p = 2; p <= 50; ++p) {
             isPrime = YES;
             for (d = 2; d < p; ++d) {
-                if (p % d == 0) {
+                if (p % d == 0)
                     isPrime = NO;
-                }
             }
-            if (isPrime == YES) {
-                NSNumber *primeNum = [NSNumber numberWithInteger:p];
-                [primes addObject:primeNum];
-            }
+            if (isPrime == YES)
+                [primes addObject:[NSNumber numberWithInteger:p]];
         }
 
-        if ([primes writeToFile:@"primes.pl" atomically:YES] == NO) {
+        if ([primes writeToFile:@"primes.pl" atomically:YES] == NO)
             NSLog(@"Save to file failed!");
-        }
-        else {
+        else
             NSLog(@"Wrote primes to file primes.pl.");
-        }
     }
 
     return 0;
